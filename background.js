2a2c0510986c0d4c6c98aede5c1f54a1d743d06d
Robert Pyzalski <robert.pyzalski@shiftboard.com>
2020-07-04 12:27:46 -0700

    getting all bookmarks, send bookmark json to server

--- background.js
+++ background.js
@@ -1,10 +1,7 @@
 function getBookmarkTree(bookmarkNode) {
     var bookmarks = [];
 
-    //console.log(bookmarkNode.title);
-
     for (child of bookmarkNode.children) {
-        //console.log(child.title);
         if (child.children === undefined) {
             bookmarks.push({ title: child.title, url: child.url});
         } else {
@@ -16,34 +13,35 @@ function getBookmarkTree(bookmarkNode) {
 }
 
 async function handleClick() {
-    //let root_title = "Bookmarks Menu";
-    let root_title = "Shiftboard";
-
-    console.log(' *** Handling click ***');
+    let root_title = "Bookmarks Menu";
 
     var bookmarks = [];
-
     try {
         // returns array of nodes
         let nodes_found = await browser.bookmarks.search({title: root_title});
         if (nodes_found.length == 0) {
-            console.error("Bookmark sync root node not found");
-            return;
+            throw("bookmark sync root node not found");
         }
+
         // uses id of first node to get the node with children
         // getSubTree returns an array with only the one node
         let root_node = await browser.bookmarks.getSubTree(nodes_found[0].id);
 
         bookmarks = getBookmarkTree(root_node[0]);
     } catch (err) {
-        console.error("Error retrieving bookmarks", err);
+        // TODO: change this to popup?
+        console.error("Error retrieving bookmarks:", err);
+        return;
     }
 
     console.log(JSON.stringify(bookmarks));
 
     //const request = new Request('http://www.orbitry.com/bookmarks/cgi/test.cgi', { method: 'POST' });
     //fetch(request);
-    fetch('http://www.orbitry.com/bookmarks/cgi/test.cgi', { method: 'POST', body: '{ "bob", "the blob" }' });
+    //fetch('http://www.orbitry.com/bookmarks/cgi/test.cgi', { method: 'POST', body: '{ "bob", "the blob" }' });
+    fetch('http://www.orbitry.com/bookmarks/cgi/test.cgi', { method: 'POST', body: JSON.stringify(bookmarks) });
+
+    //TODO: check response
 }
 
 browser.browserAction.onClicked.addListener(handleClick);
